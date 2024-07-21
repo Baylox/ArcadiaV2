ab272d54419c64267996db2a0e989234c10d2c3f
Baylospawn <microsoftbaylo@laposte.net>
2024-07-21 20:35:40 +0200

    Connection à la DB

--- Config/arcadia.sql
+++ Config/arcadia.sql
@@ -1,157 +1,151 @@
+-- Créer la base de données (si ce n'est pas déjà fait)
+CREATE DATABASE IF NOT EXISTS Arcadia;
+
+-- Utiliser la base de données
+USE Arcadia;
+
+-- Créer les tables
 CREATE TABLE Users(
-   Id_User VARCHAR(50),
-   Email_User VARCHAR(50),
-   Password_User VARCHAR(50),
+   Id_User VARCHAR(50) PRIMARY KEY,
+   Email_User VARCHAR(50) NOT NULL,
+   Password_User VARCHAR(50) NOT NULL,
    Name_User VARCHAR(50) NOT NULL,
    Firstname_User VARCHAR(50) NOT NULL,
-   Dob_User VARCHAR(50),
-   PRIMARY KEY(Id_User)
+   Dob_User DATE
 );
 
-CREATE TABLE role(
-   Id_role COUNTER,
-   label VARCHAR(50),
-   Type_User VARCHAR(50),
-   PRIMARY KEY(Id_role)
+CREATE TABLE Role(
+   Id_Role INT AUTO_INCREMENT PRIMARY KEY,
+   Label VARCHAR(50),
+   Type_User VARCHAR(50)
 );
 
-CREATE TABLE alimentation_animaux(
-   Id_alimentation INT,
-   nom_nourriture TEXT NOT NULL,
-   quantite_nourriture DECIMAL(15,4) NOT NULL,
-   date_alimentation DATETIME NOT NULL,
-   commentaire_veterinaire TEXT,
-   PRIMARY KEY(Id_alimentation)
+CREATE TABLE Alimentation_Animaux(
+   Id_Alimentation INT AUTO_INCREMENT PRIMARY KEY,
+   Nom_Nourriture TEXT NOT NULL,
+   Quantite_Nourriture DECIMAL(15,4) NOT NULL,
+   Date_Alimentation DATETIME NOT NULL,
+   Commentaire_Veterinaire TEXT
 );
 
-CREATE TABLE soins_animaux(
-   Id_traitement INT,
-   Description_soin VARCHAR(50) NOT NULL,
-   Date_soin DATETIME NOT NULL,
-   PRIMARY KEY(Id_traitement)
+CREATE TABLE Soins_Animaux(
+   Id_Traitement INT AUTO_INCREMENT PRIMARY KEY,
+   Description_Soin VARCHAR(50) NOT NULL,
+   Date_Soin DATETIME NOT NULL
 );
 
-CREATE TABLE habitat(
-   Id_habitat INT,
-   nom VARCHAR(50) NOT NULL,
-   description TEXT,
-   type_habitat VARCHAR(6) NOT NULL,
-   PRIMARY KEY(Id_habitat)
+CREATE TABLE Habitat(
+   Id_Habitat INT AUTO_INCREMENT PRIMARY KEY,
+   Nom VARCHAR(50) NOT NULL,
+   Description TEXT,
+   Type_Habitat VARCHAR(6) NOT NULL
 );
 
-CREATE TABLE image(
-   Id_image INT,
-   image_date INT,
-   PRIMARY KEY(Id_image)
+CREATE TABLE Image(
+   Id_Image INT AUTO_INCREMENT PRIMARY KEY,
+   Image_Date DATETIME NOT NULL
 );
 
-CREATE TABLE espece(
-   Id_espece INT,
-   type_espece VARCHAR(50) NOT NULL,
-   evaluation_extinction VARCHAR(50),
-   traits_cacteristiques TEXT,
-   PRIMARY KEY(Id_espece)
+CREATE TABLE Espece(
+   Id_Espece INT AUTO_INCREMENT PRIMARY KEY,
+   Type_Espece VARCHAR(50) NOT NULL,
+   Evaluation_Extinction VARCHAR(50),
+   Traits_Caracteristiques TEXT
 );
 
-CREATE TABLE race(
-   Id_race INT,
-   label VARCHAR(50),
-   description TEXT,
-   habitat_naturel VARCHAR(50),
-   PRIMARY KEY(Id_race)
+CREATE TABLE Race(
+   Id_Race INT AUTO_INCREMENT PRIMARY KEY,
+   Label VARCHAR(50),
+   Description TEXT,
+   Habitat_Naturel VARCHAR(50)
 );
 
-CREATE TABLE service(
-   Id_service COUNTER,
-   nom VARCHAR(50),
-   description_service VARCHAR(50),
-   PRIMARY KEY(Id_service)
+CREATE TABLE Service(
+   Id_Service INT AUTO_INCREMENT PRIMARY KEY,
+   Nom VARCHAR(50),
+   Description_Service TEXT
 );
 
-CREATE TABLE horaire(
-   Id_horaire INT,
-   jour_semaine DATE NOT NULL,
-   heure_debut TIME NOT NULL,
-   heure_fin TIME NOT NULL,
-   PRIMARY KEY(Id_horaire)
+CREATE TABLE Horaire(
+   Id_Horaire INT AUTO_INCREMENT PRIMARY KEY,
+   Jour_Semaine VARCHAR(10) NOT NULL,
+   Heure_Debut TIME NOT NULL,
+   Heure_Fin TIME NOT NULL
 );
 
-CREATE TABLE avis(
-   Id_avis INT,
-   pseudo VARCHAR(50),
-   commentaire VARCHAR(50),
-   status_avis VARCHAR(50) NOT NULL,
-   isVisible LOGICAL NOT NULL,
-   PRIMARY KEY(Id_avis)
+CREATE TABLE Avis(
+   Id_Avis INT AUTO_INCREMENT PRIMARY KEY,
+   Pseudo VARCHAR(50),
+   Commentaire TEXT,
+   Status_Avis VARCHAR(50) NOT NULL,
+   IsVisible BOOLEAN NOT NULL
 );
 
 CREATE TABLE Zoo(
-   Id_Zoo INT,
-   Zoo_Adress VARCHAR(50) NOT NULL,
+   Id_Zoo INT AUTO_INCREMENT PRIMARY KEY,
+   Zoo_Address VARCHAR(50) NOT NULL,
    Telephone_Number VARCHAR(15) NOT NULL,
    Id_User VARCHAR(50) NOT NULL,
-   PRIMARY KEY(Id_Zoo),
    FOREIGN KEY(Id_User) REFERENCES Users(Id_User)
 );
 
-CREATE TABLE animal(
-   Id_animal INT,
-   prenom VARCHAR(50) NOT NULL,
-   etat VARCHAR(50) NOT NULL,
-   sexe VARCHAR(7) NOT NULL,
-   Description_animal VARCHAR(50),
-   Dob_animal DATE NOT NULL,
-   Id_image INT NOT NULL,
-   Id_habitat INT NOT NULL,
-   PRIMARY KEY(Id_animal),
-   FOREIGN KEY(Id_image) REFERENCES image(Id_image),
-   FOREIGN KEY(Id_habitat) REFERENCES habitat(Id_habitat)
+CREATE TABLE Animal(
+   Id_Animal INT AUTO_INCREMENT PRIMARY KEY,
+   Prenom VARCHAR(50) NOT NULL,
+   Etat VARCHAR(50) NOT NULL,
+   Sexe VARCHAR(7) NOT NULL,
+   Description_Animal TEXT,
+   Dob_Animal DATE NOT NULL,
+   Id_Image INT NOT NULL,
+   Id_Habitat INT NOT NULL,
+   FOREIGN KEY(Id_Image) REFERENCES Image(Id_Image),
+   FOREIGN KEY(Id_Habitat) REFERENCES Habitat(Id_Habitat)
 );
 
-CREATE TABLE rapport(
-   Id_rapport INT,
-   date_rapport DATE NOT NULL,
-   details TEXT,
-   type VARCHAR(12),
+CREATE TABLE Rapport(
+   Id_Rapport INT AUTO_INCREMENT PRIMARY KEY,
+   Date_Rapport DATE NOT NULL,
+   Details TEXT,
+   Type VARCHAR(12),
    Id_User VARCHAR(50) NOT NULL,
-   Id_animal INT NOT NULL,
-   Id_traitement INT NOT NULL,
-   Id_alimentation INT NOT NULL,
-   PRIMARY KEY(Id_rapport),
+   Id_Animal INT NOT NULL,
+   Id_Traitement INT NOT NULL,
+   Id_Alimentation INT NOT NULL,
    FOREIGN KEY(Id_User) REFERENCES Users(Id_User),
-   FOREIGN KEY(Id_animal) REFERENCES animal(Id_animal),
-   FOREIGN KEY(Id_traitement) REFERENCES soins_animaux(Id_traitement),
-   FOREIGN KEY(Id_alimentation) REFERENCES alimentation_animaux(Id_alimentation)
+   FOREIGN KEY(Id_Animal) REFERENCES Animal(Id_Animal),
+   FOREIGN KEY(Id_Traitement) REFERENCES Soins_Animaux(Id_Traitement),
+   FOREIGN KEY(Id_Alimentation) REFERENCES Alimentation_Animaux(Id_Alimentation)
 );
 
-CREATE TABLE posseder(
+CREATE TABLE Posseder(
    Id_User VARCHAR(50),
-   Id_role INT,
-   PRIMARY KEY(Id_User, Id_role),
+   Id_Role INT,
+   PRIMARY KEY(Id_User, Id_Role),
    FOREIGN KEY(Id_User) REFERENCES Users(Id_User),
-   FOREIGN KEY(Id_role) REFERENCES role(Id_role)
+   FOREIGN KEY(Id_Role) REFERENCES Role(Id_Role)
 );
 
-CREATE TABLE appartenir(
-   Id_animal INT,
-   Id_espece INT,
-   PRIMARY KEY(Id_animal, Id_espece),
-   FOREIGN KEY(Id_animal) REFERENCES animal(Id_animal),
-   FOREIGN KEY(Id_espece) REFERENCES espece(Id_espece)
+CREATE TABLE Appartenir(
+   Id_Animal INT,
+   Id_Espece INT,
+   PRIMARY KEY(Id_Animal, Id_Espece),
+   FOREIGN KEY(Id_Animal) REFERENCES Animal(Id_Animal),
+   FOREIGN KEY(Id_Espece) REFERENCES Espece(Id_Espece)
 );
 
-CREATE TABLE décliner(
-   Id_espece INT,
-   Id_race INT,
-   PRIMARY KEY(Id_espece, Id_race),
-   FOREIGN KEY(Id_espece) REFERENCES espece(Id_espece),
-   FOREIGN KEY(Id_race) REFERENCES race(Id_race)
+CREATE TABLE Decliner(
+   Id_Espece INT,
+   Id_Race INT,
+   PRIMARY KEY(Id_Espece, Id_Race),
+   FOREIGN KEY(Id_Espece) REFERENCES Espece(Id_Espece),
+   FOREIGN KEY(Id_Race) REFERENCES Race(Id_Race)
 );
 
-CREATE TABLE planifier(
-   Id_service INT,
-   Id_horaire INT,
-   PRIMARY KEY(Id_service, Id_horaire),
-   FOREIGN KEY(Id_service) REFERENCES service(Id_service),
-   FOREIGN KEY(Id_horaire) REFERENCES horaire(Id_horaire)
+CREATE TABLE Planifier(
+   Id_Service INT,
+   Id_Horaire INT,
+   PRIMARY KEY(Id_Service, Id_Horaire),
+   FOREIGN KEY(Id_Service) REFERENCES Service(Id_Service),
+   FOREIGN KEY(Id_Horaire) REFERENCES Horaire(Id_Horaire)
 );
+
