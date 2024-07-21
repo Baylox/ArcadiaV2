95c048abce3c7ef138b7fe813af8bd0fe0c7b972
Baylospawn <microsoftbaylo@laposte.net>
2024-07-21 20:50:47 +0200

    Restructuration du git pour dev le Back

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,10 +2,10 @@ import Route from "./Route.js";
 
 //Définir les routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/services", "Services", "/pages/services.html"),
-    new Route("/avis", "Avis", "/pages/avis.html"),
-    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/", "Accueil", "/Public/pages/home.html"),
+    new Route("/services", "Services", "/Public//pages/services.html"),
+    new Route("/avis", "Avis", "/Public//pages/avis.html"),
+    new Route("/connexion", "Connexion", "/Public//pages/connexion.html"),
 ];
 
 export const websiteName = "Arcadia";
